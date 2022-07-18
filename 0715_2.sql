68473ef4146f4d9dc01e600867c5928ee8f239d6
thatislg <thatislg@users.noreply.github.com>
2022-07-18 23:15:12 +0900

    Finish Chapter 12,14

--- 0715_2.sql
+++ 0715_2.sql
@@ -4,8 +4,8 @@
 さい。
 */
 SELECT
-    book_id AS "書籍 ID",
-    book_name AS "書籍 ID"
+    book_id AS "書籍 ID"
+    ,book_name AS "書籍 ID"
 FROM
     book
 ORDER BY
@@ -18,31 +18,85 @@ ASC
 い。
 */
 SELECT
-    member_id,
+    member_id
 FROM
     order_header
 WHERE 
     EXTRACT(YEAR FROM order_datetime) = '2011'
 ORDER BY 
-    member_id ASC,
-    order_datetime DESC
+    member_id ASC
+    ,order_datetime DESC
 ;
 -------
 /*
-3 会員情報を非会員、正会員の順に表示しなさい。また、非会員、正会員毎に血液型の降順で、会員区分と血液型
-が同一会員に対しては、会員 ID の昇順で表示しなさい。結果表は「会員 ID]、「氏名」、「血液型」、「会員区分」を
+3 会員情報を非会員、正会員の順に表示しなさい。
+また、非会員、正会員毎に血液型の降順で、
+会員区分と血液型が同一会員に対しては、会員 ID の昇順で表示しなさい。
+結果表は「会員 ID]、「氏名」、「血液型」、「会員区分」を
 見出しとして表示しなさい。
 */
-
+SELECT
+    member_id AS "会員 ID"
+    ,first_name || last_name AS "氏名"
+    ,blood_type AS "血液型"
+    ,member_type AS "会員区分"
+FROM
+    member
+ORDER BY 
+    member_type ASC
+    ,blood_type DESC
+    ,member_id ASC
+;
+---------------
 /*
 4 カテゴリ毎の書籍の平均価格が高い順に書籍情報を表示しなさい。結果表は「カテゴリ ID」、「平均価格」を見出し
 として表示しなさい。
-5 クレジットカードを複数所持する会員の会員 ID と所持枚数の一覧を、結果表の見出しが「会員 ID」、「所持枚数」
-となるように所持枚数が多い順に表示しなさい。所持枚数が同一の会員がいる場合は、会員 ID 順で並べなさい。
+*/
+SELECT
+    category_id AS "カテゴリ ID"
+    ,ROUND(AVG(price),0) AS "平均価格"
+FROM
+    book
+GROUP BY 
+    category_id
+ORDER BY 
+    AVG(price) ASC
+;
+---------------------
+/*
+5 クレジットカードを複数所持する会員の会員 ID と所持枚数の一覧を、
+結果表の見出しが「会員 ID」、「所持枚数」となるように所持枚数が多い順に表示しなさい。
+所持枚数が同一の会員がいる場合は、会員 ID 順で並べなさい。
+*/
+SELECT
+    member_id AS "会員 ID"
+    ,COUNT(member_card_id) AS "所持枚数"
+FROM
+    member_card
+GROUP BY 
+    member_id
+ORDER BY
+    COUNT(member_card_id)DESC
+    ,member_id ASC
+;
+/*
 6 血液型毎の会員の人数の一覧を、結果表の見出しが「血液型」、「人数」となるように人数の多い順で表示しなさい。
 ただし、血液型が不明の会員については集計しない。また、人数が 2 以下の血液型は表示しない。
 */
-
+SELECT
+    blood_type AS "血液型"
+    ,COUNT(member_id) AS "人数"
+FROM
+    member
+WHERE 
+    blood_type IS NOT NULL
+GROUP BY 
+    blood_type
+HAVING 
+    COUNT(member_id) >= 2
+ORDER BY
+    COUNT(member_id) DESC
+;
 -------------------------------------
 --- CHAPTER 14
 /*
@@ -82,17 +136,75 @@ ORDER BY
     m.member_id
     ,oh.order_no
 ;
-
 /*
 3 クレジットカード情報付きの会員一覧を表示しなさい。ただし、表示対象の会員は血液型が A 型と B 型とする。結
 果表は「会員 ID」、「氏名」、「カード番号」、「有効期限年」、「有効期限月」を見出しとして表示しなさい。また、表示
 順は会員 ID の昇順で表示しなさい。会員 ID が同じレコードの中では、カード番号の昇順で表示しなさい。
+*/
+SELECT 
+    m.member_id AS "会員 ID"
+    ,m.last_name || m.first_name AS "氏名"
+    ,mc.card_number AS  "カード番号"
+    ,mc.expire_year AS "有効期限年"
+    ,mc.expire_month AS "有効期限月"
+FROM
+    member_card AS mc 
+    INNER JOIN member AS m 
+        ON m.member_id = mc.member_id
+GROUP BY 
+    m.member_id
+    ,mc.card_number
+    ,mc.expire_year
+    ,mc.expire_month
+HAVING
+    m.blood_type IN ('A', 'B')        
+ORDER BY
+    m.member_id ASC
+    ,mc.card_number ASC
+;
+-----------------
+/*
 4 東京都が所在地の出版社が出版する、書籍の一覧情報を表示しなさい。なお、出版社が特定できる書籍のみを
 表示する。結果表は「書籍 ID」、「書籍名」を見出しとして表示しなさい。表示順は書籍 ID の昇順で表示しなさい。
+*/
+SELECT 
+    book_id AS "書籍 ID"
+    ,book_name AS "書籍名"
+FROM
+    book
+WHERE 
+    publisher_id IS NOT NULL
+ORDER BY
+    book_id ASC
+;
+---------------
+/*
 5 有効期限が 2012 年のクレジットカードで決済された注文の一覧を表示しなさい。結果表は「注文番号」、「会員
 ID」、「カード番号」を見出しとして表示しなさい。
+*/
+SELECT
+    order_no AS "注文番号"
+    ,member_id AS "会員ID"
+    ,member_card_id AS "カード番号"
+FROM
+    order_header
+WHERE   
+    EXTRACT(YEAR FROM order_datetime) = '2012'
+;
+--------------
+/*
 6 出版社の所在地毎に書籍の出版数(書籍の数)の一覧を、結果表の見出しが「所在地」、「出版数」となるように表
 示しなさい。なお、出版数が 1 以上の所在地のみを表示すればよい。列の見出しはそれぞれ「所在地」、「出版数」
 としなさい。
 */
-    
+
+SELECT
+    prefecture AS "所在地"
+    ,COUNT(publisher_id) AS "出版数"
+FROM
+    publisher
+GROUP BY 
+    prefecture
+HAVING
+    COUNT(publisher_id) >= 1
+;
